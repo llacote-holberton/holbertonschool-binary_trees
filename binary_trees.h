7a7271a3e251f7c2848a87ff102bbde90cf35fff
Laurent Lacôte <laurent.lacote@gmail.com>
2026-04-06 14:25:32 +0200

    C - BinTrees - Initializing the custom header.
    - Integrated provided structures.
    - Set up placeholders for each exercise's prototype
      (ease up git diff and potential conflict resolution).

---
+++ binary_trees.h
@@ -0,0 +1,74 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/* === Externally provided resources === */
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree variant */
+typedef struct binary_tree_s bst_t;
+
+/* Adelson-Velsky and Landis Variant */
+typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heapt variant */
+typedef struct binary_tree_s heap_t;
+
+/* === Exercises prototypes === */
+
+/* BinTrees - Exo 00 - 0-binary_tree_node.c */
+
+/* BinTrees - Exo 01 - 1-binary_tree_insert_left.c */
+
+/* BinTrees - Exo 02 - 2-binary_tree_insert_right.c */
+
+/* BinTrees - Exo 03 - 3-binary_tree_delete.c */
+
+/* BinTrees - Exo 04 - 4-binary_tree_is_leaf.c */
+
+/* BinTrees - Exo 05 - 5-binary_tree_is_root.c */
+
+/* BinTrees - Exo 06 - 6-binary_tree_preorder.c */
+
+/* BinTrees - Exo 07 - 7-binary_tree_inorder.c */
+
+/* BinTrees - Exo 08 - 8-binary_tree_postorder.c */
+
+/* BinTrees - Exo 09 - 9-binary_tree_height.c */
+
+/* BinTrees - Exo 10 - 10-binary_tree_depth.c */
+
+/* BinTrees - Exo 11 - 11-binary_tree_size.c */
+
+/* BinTrees - Exo 12 - 12-binary_tree_leaves.c */
+
+/* BinTrees - Exo 13 - 13-binary_tree_nodes.c */
+
+/* BinTrees - Exo 14 - 14-binary_tree_balance.c */
+
+/* BinTrees - Exo 15 - 15-binary_tree_is_full.c */
+
+/* BinTrees - Exo 16 - 16-binary_tree_is_perfect.c */
+
+/* BinTrees - Exo 17 - 17-binary_tree_sibling.c */
+
+/* BinTrees - Exo 18 - 18-binary_tree_uncle.c */
+
+
+#endif
