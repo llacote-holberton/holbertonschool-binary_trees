cb74e18ace1811e713761dfd2120f79db8c61d51
Laurent Lacôte <laurent.lacote@gmail.com>
2026-04-08 14:57:21 +0200

    C - BinTrees - Exo 08 - Applying func on tree "post-order"
    COMPILER COMMAND
    gcc -Wall -Wextra -Werror -pedantic -std=gnu89
      binary_tree_print.c 8-main.c
      8-binary_tree_postorder.c 0-binary_tree_node.c
      -o BinTrees_Exo-O8.out
    
    NOTE: post-order means bottom-up left then bottom-up right
      and finish with parent.

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,29 @@
+#include <stddef.h>        /* Required for NULL.   */
+#include <stdlib.h>        /* Required for malloc. */
+#include <stdio.h>         /* Required for printf. */
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Processes func on tree "post-order".
+ * @tree: tree to traverse fully.
+ * @func: function to call upon each found node.
+ * Return: nothing.
+ *
+ * NOTES:
+ * If tree or func is NULL, do nothing.
+ * Post-order means left bottom-up, right bottom-up
+ *   finally parent.
+ *
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	/* Preorder traversal means we all "all left first starting with root".*/
+	if (tree->left)
+		binary_tree_postorder(tree->left, func);
+	if (tree->right)
+		binary_tree_postorder(tree->right, func);
+	printf("%d\n", tree->n);
+}
+
--- binary_trees.h
+++ binary_trees.h
@@ -51,7 +51,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 /* BinTrees - Exo 07 - 7-binary_tree_inorder.c */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 /* BinTrees - Exo 08 - 8-binary_tree_postorder.c */
-
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 /* BinTrees - Exo 09 - 9-binary_tree_height.c */
 
 /* BinTrees - Exo 10 - 10-binary_tree_depth.c */
